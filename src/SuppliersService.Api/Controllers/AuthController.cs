237ce1335ce6403be1ab1f4b31ca2ec8db246b16
Charlietzu <caiocesarsilva08@gmail.com>
2021-05-14 22:50:41 -0300

    Authentication Controller

---
+++ src/SuppliersService.Api/Controllers/AuthController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SuppliersService.Api.ViewModels;
+using SuppliersService.Business.Interfaces;
+using System.Threading.Tasks;
+
+namespace SuppliersService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : MainController
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
+        public AuthController(INotificator notificator, SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager) : base(notificator)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult> Register(RegisterUserViewModel registerUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            IdentityUser user = new IdentityUser
+            {
+                UserName = registerUser.Email,
+                Email = registerUser.Email,
+                EmailConfirmed = true
+            };
+
+            IdentityResult result = await _userManager.CreateAsync(user, registerUser.Password);
+
+            if (result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return CustomResponse(registerUser);
+            }
+
+            foreach (IdentityError error in result.Errors)
+            {
+                NotifyError(error.Description);
+            }
+
+            return CustomResponse(registerUser);
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult> Login(LoginUserViewModel loginUser)
+        {
+            if (!ModelState.IsValid) return CustomResponse(ModelState);
+
+            var result = await _signInManager.PasswordSignInAsync(loginUser.Email, loginUser.Password, false, true);
+
+            if (result.Succeeded)
+            {
+                return CustomResponse(loginUser);
+            }
+
+            if (result.IsLockedOut)
+            {
+                NotifyError("User blocked by too many wrong attempts");
+                return CustomResponse(loginUser);
+            }
+
+            NotifyError("Invalid user or password");
+            return CustomResponse(loginUser);
+        }
+    }
+}
---
+++ src/SuppliersService.Api/ViewModels/UserViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SuppliersService.Api.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "The field {0} is in a invalid format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(100, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
+        
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+
+    public class LoginUserViewModel
+    {
+        [Required(ErrorMessage = "The field {0} is required")]
+        [EmailAddress(ErrorMessage = "The field {0} is in a invalid format")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "The field {0} is required")]
+        [StringLength(100, ErrorMessage = "The field {0} must be between {2} and {1} characters", MinimumLength = 6)]
+        public string Password { get; set; }
+    }
+}
